29b9778cbd52563c737ae68cd9fa0d562c90d898
Il'ya (Marshal) <ilya@marshal.by>
2022-01-25 01:08:49 +0100

    fix path to patches

--- docker/build_scripts/maybe-patch-cpython.sh
+++ docker/build_scripts/maybe-patch-cpython.sh
@@ -4,17 +4,22 @@
 # Stop at any error, show all commands
 set -exuo pipefail
 
+# Get script directory
+MY_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+PATCHES_DIR=$MY_DIR/cpython-patches
+
 CPYTHON_VERSION=$1
 CPYTHON_SRC_DIR=Python-${CPYTHON_VERSION}
 
 if [[ "${CPYTHON_VERSION}" == 3.6* ]]; then
-  patch -s -p0 -d "${CPYTHON_SRC_DIR}" < cpython-patches/bpo-44751-cp36.diff
+  patch -s -p0 -d "${CPYTHON_SRC_DIR}" < $PATCHES_DIR/bpo-44751-cp36.diff
 fi
 
 if [[ "${CPYTHON_VERSION}" == 3.7* ]]; then
-  patch -s -p0 -d "${CPYTHON_SRC_DIR}" < cpython-patches/bpo-44751-cp37.diff
+  patch -s -p0 -d "${CPYTHON_SRC_DIR}" < $PATCHES_DIR/bpo-44751-cp37.diff
 fi
 
 if [[ "${CPYTHON_VERSION}" == 3.8* ]]; then
-  patch -s -p0 -d "${CPYTHON_SRC_DIR}" < cpython-patches/bpo-44751-cp38.diff
+  patch -s -p0 -d "${CPYTHON_SRC_DIR}" < $PATCHES_DIR/bpo-44751-cp38.diff
 fi
